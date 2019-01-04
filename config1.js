9c96ba22dfce3c70b6ce81e326696068b19d65dc
illusiveo <46373282+illusiveo@users.noreply.github.com>
2019-01-04 14:21:58 +0300

    Add files via upload

---
+++ config1.js
@@ -0,0 +1,5 @@
+exports.TOKEN = 'process.env.BOT_TOKEN'; // حط توكن البوت هنا
+
+exports.PREFIX = '2';//برفكس البوت هو يلي بيشغل لاغاني
+
+exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
